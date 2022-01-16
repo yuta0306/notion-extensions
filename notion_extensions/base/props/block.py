97e91c65c6391dc884ae2921471bae04706726af
yuta0306 <you-2001-3-6@ezweb.ne.jp>
2022-01-16 21:36:29 +0900

    add table of contents

--- notion_extensions/base/props/block.py
+++ notion_extensions/base/props/block.py
@@ -2068,3 +2068,53 @@ class Divider(Block):
 
     def __init__(self):
         super().__init__()
+
+
+class TableOfContents(Block):
+    """
+    TableOfContents
+    TableOfContents property values of block
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+    clear()
+        Clear data of title
+    json()
+        Return this class as dictionary
+    """
+
+    TEMPLATE: Dict[str, Union[str, Dict]] = {
+        "type": "table_of_contents",
+        "table_of_contents": {},
+    }
+
+    def __init__(self):
+        super().__init__()
+
+
+class BreadCrumb(Block):
+    """
+    BreadCrumb
+    BreadCrumb property values of block
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+    clear()
+        Clear data of title
+    json()
+        Return this class as dictionary
+    """
+
+    TEMPLATE: Dict[str, Union[str, Dict]] = {
+        "type": "breadcrumb",
+        "breadcrumb": {},
+    }
+
+    def __init__(self):
+        super().__init__()
